91c2ab6c80684c531c9ff32580bf11d35c054c08
dicalj <58870221+dicalj@users.noreply.github.com>
2022-05-16 19:28:46 -0400

    feat: add sum (#1)
    * feat: add sum
    
    * chore: codecov version

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,5 +1,9 @@
-import { printName } from './example'
+import { printName, sum } from "./example";
 
 test("make sure the first name plus the last name equal the full name", () => {
   expect(printName("Hanna", "Baum")).toBe("Hanna Baum");
+});
+
+test("should add between two numbers", () => {
+  expect(sum(1, 1)).toBe(2);
 });
--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,7 @@
 export function printName(firstName: string, lastName: string) {
   return `${firstName} ${lastName}`;
 }
+
+export function sum(a: number, b: number) {
+  return a + b;
+}
